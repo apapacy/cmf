9d7e7ff98a32200dba30460f545b629e45e7a672
Андрей Овчаренко <ovcharenkoav@meta.ua>
2017-05-13 01:58:22 +0300

    some fix

---
+++ src/Acme/DemoBundle/DataFixtures/PHPCR/LoadPageData.php
@@ -0,0 +1,37 @@
+<?php
+namespace Acme\DemoBundle\DataFixtures\PHPCR;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\ODM\PHPCR\DocumentManager;
+use Symfony\Cmf\Bundle\SimpleCmsBundle\Doctrine\Phpcr\Page;
+
+class LoadPageData implements FixtureInterface, OrderedFixtureInterface
+{
+    public function getOrder()
+    {
+        // refers to the order in which the class' load function is called
+        // (lower return values are called first)
+        return 10;
+    }
+
+    public function load(ObjectManager $documentManager)
+    {
+        if (!$documentManager instanceof DocumentManager) {
+            $class = get_class($documentManager);
+            throw new \RuntimeException("Fixture requires a PHPCR ODM DocumentManager instance, instance of '$class' given.");
+        }
+        $page = new Page(); // create a new Page object (document)
+        $page->setName('new_page'); // the name of the node
+        $page->setLabel('Another new Page');
+        $page->setTitle('Another new Page');
+        $page->setBody('I have added this page myself!');
+
+        $simpleCmsRoot = $documentManager->find(null, '/cms/simple');
+        $page->setParentDocument($simpleCmsRoot); // set the parent to the root
+
+        $documentManager->persist($page); // add the Page in the queue
+        $documentManager->flush(); // add the Page to PHPCR
+    }
+}
